29f1e9b2dd7d8e8bc39c39af103a8767bf9cb233
dnyaneshwarimalunjkar4-netizen <dnyaneshwarimalunjkar4@gmail.com>
2026-05-28 09:35:26 +0530

    Update problem_1.py

--- problem_1.py
+++ problem_1.py
@@ -1,10 +1,11 @@
-# Solve the following problem:
-# Write a function to calculate the number of seconds in a week
+#
 
-# Write code here
+)
 
-def seconds_in_week():
+print(seconds_iconds
 
-  return(result)
-
-print(seconds_in_week)
+a=६०
+b=२४*b
+d=७*c
+result=d
+return(result) 
